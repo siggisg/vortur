2002cc4c40d47ff67370ec12ed7559ff52dd7519
siggisg <siggisg@gmail.com>
2026-07-10 11:42:51 +0200

    Fixed text in SWIFT intro

--- src/components/WhatisIntro.jsx
+++ src/components/WhatisIntro.jsx
@@ -3,8 +3,8 @@ import '../styles/whatisintro.css'
 function whatisintro() {
     return (
       <div className="whatisintro">
-        <h1 className='whatisintro-headline'>Hvað gerum við?</h1>
-        <p className='whatisintro-text'>Við fjarlægjum vörtur á þægilegri máta með vél sem heitir SWIFT. Meðferðin er fljótleg, tekur aðeins nokkrar mínútur og krefst engrar skurðaðgerðar né batatíma. Þú getur því haldið áfram þínu daglega lífi strax að meðferð lokinni.
+        <h1 className='whatisintro-headline'>Hvað gerir SWIFT?</h1>
+        <p className='whatisintro-text'>SWIFT er vél sem er notuð til að fjarlægja vörtur á þægilegri máta. Meðferðin er fljótleg, tekur aðeins nokkrar mínútur og krefst engrar skurðaðgerðar né batatíma. Þú getur því haldið áfram þínu daglega lífi strax að meðferð lokinni.
 Oftast þarf 3–4 meðferðir með nokkurra vikna millibili til að ná bestum árangri.
 Hentar sérstaklega vel fyrir iljarvörtur og vörtur sem hafa reynst erfiðar viðureignar.</p>
       </div>
